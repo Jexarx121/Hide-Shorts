afb4bc1d8c7210950cd5b35ba2de67cea70aeb9c
jexarx <zjchen402@gmail.com>
2026-05-23 20:12:01 +0100

    Added optional to enable shorts in subscriptions

--- content.js
+++ content.js
@@ -1,9 +1,29 @@
 const CLASS = 'hide-shorts-enabled';
+const SUBSCRIPTION_CLASS = 'hide-shorts-subscription-enabled';
+const ON_SUBSCRIPTION_PAGE_CLASS = 'on-subscriptions-class';
 
-browser.storage.local.get({ enabled: true }, ({ enabled }) => {
+function updatePageClass() {
+    document.documentElement.classList.toggle(ON_SUBSCRIPTION_PAGE_CLASS, location.pathname === '/feed/subscriptions');
+};
+
+// Watch for YouTube SPA navigation
+const observer = new MutationObserver(updatePageClass);
+observer.observe(document.querySelector('title') ?? document.documentElement, {
+    childList: true, subtree: true
+});
+updatePageClass();
+
+browser.storage.local.get({ enabled: true, hideInSubscriptions: false }, ({ enabled, hideInSubscriptions }) => {
     document.documentElement.classList.toggle(CLASS, enabled);
+    document.documentElement.classList.toggle(SUBSCRIPTION_CLASS, hideInSubscriptions);
 });
 
-browser.runtime.onMessage.addListener(({ enabled }) => {
-    document.documentElement.classList.toggle(CLASS, enabled);
+browser.runtime.onMessage.addListener((msg) => {
+    if ('enabled' in msg) {
+        document.documentElement.classList.toggle(CLASS, msg.enabled);
+    }
+
+    if ('hideInSubscriptions' in msg) {
+        document.documentElement.classList.toggle(SUBSCRIPTION_CLASS, msg.hideInSubscriptions);
+    }
 });
--- popup.js
+++ popup.js
@@ -1,8 +1,14 @@
 const toggle = document.getElementById('toggle');
+const toggleSubscription = document.getElementById('toggle-subscriptions');
 
 browser.storage.local.get({ enabled: true }, ({ enabled }) => {
     toggle.checked = enabled;
 });
+
+// Hiding in subscriptions is false by default
+browser.storage.local.get({ hideInSubscriptions: false }, ({ hideInSubscriptions }) => {
+    toggleSubscription.checked = hideInSubscriptions;
+})
 
 // Goes through tabs to find active youtube tab
 toggle.addEventListener('change', () => {
@@ -12,3 +18,12 @@ toggle.addEventListener('change', () => {
         browser.tabs.sendMessage(tab.id, { enabled }).catch(() => { });
     });
 });
+
+toggleSubscription.addEventListener('change', () => {
+    console.log(toggleSubscription.checked);
+    const hideInSubscriptions = toggleSubscription.checked;
+    browser.storage.local.set({ hideInSubscriptions });
+    browser.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
+        browser.tabs.sendMessage(tab.id, { hideInSubscriptions }).catch(() => { });
+    });
+})
